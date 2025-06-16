be1f6bc9babba1c9cba4ef339d05d60d0be3b117
satyambhardwaj59 <satyambhardwaj59@gmail.com>
2025-06-16 20:29:33 +0530

    Feat : Build SpecialityMenu and Top Doctors section in Home Page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Header from '../componentes/Header'
+import SpecialityMenu from './../componentes/SpecialityMenu';
+import TopDoctors from './../componentes/TopDoctors';
 
 const Home = () => {
   return (
     <div>
         <Header />
+        <SpecialityMenu />
+        <TopDoctors />
       
     </div>
   )
