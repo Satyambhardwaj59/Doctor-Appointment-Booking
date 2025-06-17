24e3d8b297f409b33c94bc61a3e60506784ae939
satyambhardwaj59 <satyambhardwaj59@gmail.com>
2025-06-17 22:58:35 +0530

    Feat : Update footer with onClick navigate property on home, about and contact page

--- frontend/src/componentes/Footer.jsx
+++ frontend/src/componentes/Footer.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import { assets } from '../assets/assets';
+import { useNavigate } from 'react-router-dom';
 
 const Footer = () => {
+    const navigate = useNavigate();
   return (
     <div className='md:mx-10'>
         <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
@@ -16,10 +18,10 @@ const Footer = () => {
             <div>
                 <p className='text-xl font-medium mb-5'>COMPANY</p>
                 <ul className='flex flex-col gap-2 text-gray-600'>
-                    <li>Home</li>
-                    <li>About us</li>
-                    <li>Contact us</li>
-                    <li>Privacy policy</li>
+                    <li onClick={() =>{navigate('/'); scrollTo(0,0)}} className='cursor-pointer'>Home</li>
+                    <li onClick={() =>{navigate('/about'); scrollTo(0,0)}} className='cursor-pointer'>About us</li>
+                    <li onClick={() =>{navigate('/contact'); scrollTo(0,0)}} className='cursor-pointer'>Contact us</li>
+                    <li className='cursor-pointer'>Privacy policy</li>
                 </ul>
 
             </div>
