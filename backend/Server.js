f9b562ebcf4702ddf0801e1684a8ceaa5310cc2f
satyambhardwaj59 <satyambhardwaj59@gmail.com>
2025-06-20 10:48:16 +0530

    Config : in Backend config Server, DB and Cloudinary , setup .env

---
+++ backend/Server.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import connectDB from './config/mongodb.js';
+import connectCloudinary from './config/cloudinary.js';
+
+// app config
+const app = express();
+const port = process.env.PORT || 4000;
+connectDB();
+connectCloudinary();
+
+// middlewares 
+app.use(express.json());
+app.use(cors());
+
+// api endpoints
+
+app.get('/', (req, res) => {
+    res.send('API working , form hello world')
+    
+});
+
+app.listen(port, () => {
+    console.log('Server is running on port: ', port);
+})
+
+// connectDB().then(() => {
+//     app.listen(port, () => {
+//         console.log('Server is running on port: ', port);
+//     });
+// }).catch(err => {
+//     console.error('Database connection failed:', err);
+// })
