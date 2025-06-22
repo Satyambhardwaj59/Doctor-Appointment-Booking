6d3a16bc789ecd12af4f1dc8853bb78d8476a695
satyambhardwaj59 <satyambhardwaj59@gmail.com>
2025-06-22 10:24:35 +0530

    Feat : Build Sidebar and Add Doctor UI in admin panel

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -3,6 +3,12 @@ import Login from './pages/Login';
 import { ToastContainer } from 'react-toastify';
 import { AdminContext } from './context/AdminContext';
 import Navbar from './components/Navbar';
+import Sidebar from './components/Sidebar';
+import { Route, Routes } from 'react-router-dom';
+import Dashboard from './pages/admin/Dashboard';
+import AllApointments from './pages/admin/AllApointments';
+import AddDoctor from './pages/admin/AddDoctor';
+import DoctorsList from './pages/admin/DoctorsList';
 
 const App = () => {
 
@@ -13,6 +19,16 @@ const App = () => {
       
       <ToastContainer/>
       <Navbar/>
+      <div className='flex items-start'>
+        <Sidebar />
+        <Routes>
+          <Route path='/' element={<></>} />
+          <Route path='/admin-dashboard' element={<Dashboard />} />
+          <Route path='/all-appointments' element={<AllApointments />} />
+          <Route path='/add-doctor' element={<AddDoctor />} />
+          <Route path='/doctors-list' element={<DoctorsList />} />
+        </Routes>
+      </div>
     </div>
   ) : (
     <>
