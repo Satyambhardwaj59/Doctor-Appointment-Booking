c8de5ab2087553fb386e1cc5b257a3a0c75b4205
satyambhardwaj59 <satyambhardwaj59@gmail.com>
2025-06-29 10:41:17 +0530

    Feat : Build api to getting all appointments data in admin panel and desplay it in admin panel

--- backend/routes/adminRoute.js
+++ backend/routes/adminRoute.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { addDoctor, allDoctors, loginAdmin } from '../controllers/adminController.js';
+import { addDoctor, allDoctors, appoimtmentsAdmin, loginAdmin } from '../controllers/adminController.js';
 import upload from '../middleware/multer.js';
 import authAdmin from '../middleware/authAdmin.js';
 import { changeAvailablity } from '../controllers/doctorController.js';
@@ -10,5 +10,6 @@ adminRouter.post('/add-doctor', authAdmin, upload.single('image'), addDoctor);
 adminRouter.post('/login', loginAdmin);
 adminRouter.post('/all-doctors', authAdmin, allDoctors);
 adminRouter.post('/change-availability', authAdmin, changeAvailablity);
+adminRouter.get('/appointments', authAdmin, appoimtmentsAdmin);
 
 export default adminRouter
