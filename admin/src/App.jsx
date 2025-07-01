e9381dde29889e5303f858b78af98e0e605e44ad
satyambhardwaj59 <satyambhardwaj59@gmail.com>
2025-07-01 09:00:35 +0530

    Feat : Build Doctor Panel

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -9,12 +9,17 @@ import Dashboard from './pages/admin/Dashboard';
 import AllApointments from './pages/admin/AllApointments';
 import AddDoctor from './pages/admin/AddDoctor';
 import DoctorsList from './pages/admin/DoctorsList';
+import { DoctorContext } from './context/DoctorContext';
+import DoctorDashboard from './pages/doctor/DoctorDashboard';
+import DoctorAppointments from './pages/doctor/DoctorAppointments';
+import DoctorProfile from './pages/doctor/DoctorProfile';
 
 const App = () => {
 
-  const {aToken} = useContext(AdminContext)
+  const {aToken} = useContext(AdminContext);
+  const {dToken} = useContext(DoctorContext)
 
-  return aToken ? (
+  return aToken || dToken ? (
     <div className='bg-[#F8F9FD] '>
       
       <ToastContainer/>
@@ -22,11 +27,17 @@ const App = () => {
       <div className='flex items-start'>
         <Sidebar />
         <Routes>
+          {/* Admin Route */}
           <Route path='/' element={<></>} />
           <Route path='/admin-dashboard' element={<Dashboard />} />
           <Route path='/all-appointments' element={<AllApointments />} />
           <Route path='/add-doctor' element={<AddDoctor />} />
           <Route path='/doctors-list' element={<DoctorsList />} />
+
+          {/* Doctor Route */}
+          <Route path='/doctor-dashboard' element={<DoctorDashboard />} />
+          <Route path='/doctor-appointments' element={<DoctorAppointments />} />
+          <Route path='/doctor-profile' element={<DoctorProfile />} />
         </Routes>
       </div>
     </div>
