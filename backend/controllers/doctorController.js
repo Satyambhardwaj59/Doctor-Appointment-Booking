d8f066c277108b58af2104994583c5a6e294ce61
satyambhardwaj59 <satyambhardwaj59@gmail.com>
2025-07-01 20:46:29 +0530

    Feat : Build API for Doctor Dashboard and make it functional

--- backend/controllers/doctorController.js
+++ backend/controllers/doctorController.js
@@ -120,6 +120,45 @@ const appointmentsDoctor = async (req, res) => {
       console.log(error);
       res.json({success: false, message: error.message })
     }
+ };
+
+ // API to get dashboard data for doctor panel
+ const doctorDashboard = async (req, res) => {
+    try {
+
+      const {docId} = req.user;
+
+      const appointments = await appointmentModel.find({docId});
+
+      let earnings = 0;
+
+      appointments.map((item) => {
+        if (item.isCompleted || item.payment) {
+          earnings += item.amount;
+        }
+      })
+
+      let patients = [];
+
+      appointments.map((item) => {
+        if (!patients.includes(item.userId)) {
+          patients.push(item.userId)
+        }
+      })
+
+      const dashData = {
+        earnings,
+        appointments: appointments.length,
+        patients: patients.length,
+        latestAppointments: appointments.reverse().slice(0, 5)
+      }
+
+      res.json({success: true, dashData})
+      
+    } catch (error) {
+      console.log(error);
+      res.json({success: false, message: error.message })
+    }
  }
 
-export {changeAvailablity, doctorList, loginDoctor, appointmentsDoctor, appointmentComplete, appointmentCancel}
+export {changeAvailablity, doctorList, loginDoctor, appointmentsDoctor, appointmentComplete, appointmentCancel, doctorDashboard}
